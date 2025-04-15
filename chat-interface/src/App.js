d33ea3bcf34dc335e97b07ed3d1b4ded6c15ae9f
himanshu-kharkwal <hkharkwal92@gmail.com>
2025-04-15 17:49:24 +0530

    Update App.js

--- chat-interface/src/App.js
+++ chat-interface/src/App.js
@@ -15,7 +15,7 @@ const ChatInterface = () => {
     setLoading(true);
 
     try {
-      const response = await fetch("https://ykvv4099qe.execute-api.ap-south-1.amazonaws.com/prod/chat-bot/teted", {
+      const response = await fetch("https://eirrvqw7zl.execute-api.ap-south-1.amazonaws.com/testltstAPI/testltst", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
